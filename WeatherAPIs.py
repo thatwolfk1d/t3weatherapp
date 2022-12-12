b63a2fa3b22bd59d7a2edf5390d791a935bdee5b
bradyshu <85517338+bradyshu@users.noreply.github.com>
2022-12-12 17:55:00 -0500

    Update WeatherAPIs.py
    the hourly and current forecast now return a list of dictionaries, with no sub-dictionaries. Each dictionary has a "header" value to indicate its original key when it was a sub-dictionary.

--- WeatherAPIs.py
+++ WeatherAPIs.py
@@ -5,14 +5,14 @@ Purpose: This program is a series of API calls to OpenWeatherAPI for Location an
 """
 
 import requests
+from typing import List
 
 API_SECRET = '&appid=c498ab6233adc1ec28ededc44418df6a'
 
-
-''' 
+'''
 this function converts zipcode and country code to lat-lon coordinates to build 
 '''
-def get_location_zip(x,y):
+def get_location_zip(x, y):
     location_api_url = 'https://api.openweathermap.org/geo/1.0/zip?zip='
     zip_code = x
     country_code = y
@@ -25,10 +25,13 @@ def get_location_zip(x,y):
     coordinates = "lat=" + str(latitude_cords) + "&" + "lon=" + str(longitude_cords)
     return coordinates
 
+
 """
 This function converts a city name into lat-lon coordinates to build
 """
-def get_location_city(x,y):
+
+
+def get_location_city(x, y):
     location_api_url = 'http://api.openweathermap.org/geo/1.0/direct?q='
     city_code = x
     country_code = y
@@ -42,12 +45,15 @@ def get_location_city(x,y):
     coordinates = "lat=" + str(latitude_cords) + "&" + "lon=" + str(longitude_cords)
     return coordinates
 
+
 """
 This function takes in two values, the first is either a zip code or city name, the second is a two character country code
 such as GB (great britain) FR (france) or US ( United states)
 it then returns coordinate for use by the weather fetchers
 """
-def get_location(x,y):
+
+
+def get_location(x, y):
     try:
         coordinate = get_location_zip(x, y)
         return coordinate
@@ -57,44 +63,82 @@ def get_location(x,y):
         coordinate = get_location_city(x, y)
         return coordinate
     except:
-        return("INVALID INPUT")
+        return ("INVALID INPUT")
+
 
 """
 this function calls the OpenWeatherAPI for current forecast data
 """
-def get_current_forecast(x,y):
+
+
+def get_current_forecast(x, y):
     current_api_url = 'https://pro.openweathermap.org/data/2.5/weather?'
-    build_current_api_url = current_api_url + get_location(x,y) + API_SECRET
+    build_current_api_url = current_api_url + get_location(x, y) + API_SECRET
     current_weather_response = requests.get(build_current_api_url)
     current_weather_data = current_weather_response.json()
-    return current_weather_data
+    mylist = []
+    for key in current_weather_data:
+        if type(current_weather_data[key]) == dict:
+            mydict = {}
+            mydict["Header"]=key
+            mydict.update(current_weather_data[key])
+            mylist.append(mydict)
+        else:
+            mydict = {"Header":key,key:current_weather_data[key]}
+            mylist.append(mydict)
+
+    return mylist
+
 
 """
 this function calls the OpenWeatherAPI for hourly forecast data
 """
-def get_hourly_forecast(x,y):
+
+
+def get_hourly_forecast(x, y):
     hourly_api_url = 'https://pro.openweathermap.org/data/2.5/forecast/hourly?'
-    build_hourly_api_url = hourly_api_url + get_location(x,y) + API_SECRET
+    build_hourly_api_url = hourly_api_url + get_location(x, y) + API_SECRET
     hourly_weather_response = requests.get(build_hourly_api_url)
     hourly_weather_data = hourly_weather_response.json()
-    return hourly_weather_data
+    mylist = []
+    for key in hourly_weather_data:
+        if key == "list":
+            for item in hourly_weather_data[key]:
+                mydict = {}
+                mydict["Header"]=list(item.values())[-1]
+                mydict.update(item)
+                mylist.append(mydict)
+        elif type(hourly_weather_data[key]) == dict:
+            mydict = {}
+            mydict["Header"]=key
+            mydict.update(hourly_weather_data[key])
+            mylist.append(mydict)
+
+    return mylist
+
 
 """
 this function calls the OpenWeatherAPI for daily forecast data
 """
-def get_daily_forecast(x,y):
+
+
+def get_daily_forecast(x, y):
     daily_api_url = 'https://pro.openweathermap.org/data/2.5/forecast/daily?'
-    build_daily_api_url = daily_api_url + get_location(x,y) + API_SECRET
+    build_daily_api_url = daily_api_url + get_location(x, y) + API_SECRET
     daily_weather_response = requests.get(build_daily_api_url)
     daily_weather_data = daily_weather_response.json()
     return daily_weather_data
 
+
 """
 this function calls the OpenWeatherAPI for monthly forecast data
 """
-def get_monthly_forecast(x,y):
+
+
+def get_monthly_forecast(x, y):
     monthly_api_url = 'https://pro.openweathermap.org/data/2.5/forecast/climate?'
-    build_monthly_api_url = monthly_api_url + get_location(x,y) + API_SECRET
+    build_monthly_api_url = monthly_api_url + get_location(x, y) + API_SECRET
     monthly_weather_response = requests.get(build_monthly_api_url)
     monthly_weather_data = monthly_weather_response.json()
     return monthly_weather_data
+
